3a5df9dd8b92fa50bd00eddd4e8486580de2d403
Moises Protazio <moisesprotazio@gmail.com>
2026-04-25 16:30:52 -0300

    Estendi o cliente para chamar os novos métodos do objeto servidor

--- Client.java
+++ Client.java
@@ -7,7 +7,7 @@ public class Client {
 
             // 2. Create a proxy to the remote 'Printer' object
             // Replace '10.0.0.5' with the actual IP of your ICE server
-            ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -h 98.90.53.6 -p 5678");
+            ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -h localhost -p 11000"); // mudei pra porta do servidor
 
             // 3. Downcast the proxy to the Printer interface
             Demo.PrinterPrx printer = Demo.PrinterPrx.checkedCast(base);
@@ -19,6 +19,29 @@ public class Client {
             // 4. Call the remote method
             String response = printer.printString("Hello from Goiania!");
             System.out.println("Server responded: " + response);
+
+            int sum = printer.add(1,2);
+            int sub = printer.sub(5,10);
+
+            System.out.println("Soma: " + sum);
+            System.out.println("Subtração: " + sub);
+
+
+            //Novo objeto criado para calor
+            ObjectPrx baseCalor = communicator.stringToProxy("MyCalor:default -h localhost -p  11000");
+            
+            Demo.CalorPrx calor = Demo.CalorPrx.checkedCast(baseCalor);
+
+            if (calor == null) {
+                throw new Error("Invalid proxy");
+            }
+
+            double calorSensivel = calor.calorSensivel(10, 1, 50);
+            double calorLatente = calor.calorLatente(1400, 540);
+
+            System.out.println("Calor Sensível: " + calorSensivel);
+            System.out.println("Calor Latente: " + calorLatente);
+
 
         } catch (LocalException e) {
             e.printStackTrace();
--- Printer.ice
+++ Printer.ice
@@ -3,5 +3,18 @@ module Demo
     interface Printer
     {
         string printString(string s);
+            
+        int add(int a, int b);
+
+        int sub(int a, int b);
+    }
+
+    interface Calor
+    {
+
+        double calorSensivel(double m, double c, double teta);
+
+        double calorLatente(double m, double L);
+
     }
 }
